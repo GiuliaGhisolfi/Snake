4498a4520b1c220c1d9b9998b5577306d0113181
giuliaghisolfi99@gmail.com <giuliaghisolfi99@gmail.com>
2022-12-14 17:01:40 +0100

    food comment

--- food.py
+++ food.py
@@ -6,16 +6,19 @@ class Food:
         self.position = None
 
     def draw(self, game, window, grid):
+        """draw food"""
         [x,y] = self.coord_from_graph(grid)
         radious = (grid.block_size / 2) - 3
         game.draw.circle(
             window, self.color, (x + radious + 3, y + radious + 3), radious)
         
     def coord_from_graph(self, grid):
+        """returns the coordinates on the window in pixels given the node in the graph"""
         node = self.position
         return (node[0]*grid.block_size, node[1]*grid.block_size)
 
     def is_overlapped(self, position, snakes, grid):
+        """returns true if the food position is on the snake or on an obstacle"""
         for s in snakes:
             for segment in s.get_body():
                 if segment == position:
@@ -27,22 +30,23 @@ class Food:
         return False
 
     def respawn(self, snakes, grid):
+        """generete new food position s.t. it is inside the game grid, not overlapping the snake or obstacles"""
         def distance(n1, n2):
             return abs(n1[0]-n2[0]) + abs(n1[0] - n2[0])
 
         nodes = list(grid.grid.keys())
-        rand.shuffle(nodes)
+        rand.shuffle(nodes) # random reorganize the order of the nodes
         
-        scartati = []
+        possible_position = []
         for n in nodes:
             skip = False
             for s in snakes:
                 if distance(n, s.get_body()[1]) <= 1:
                     skip = True
-                    scartati.append(n)
+                    possible_position.append(n)
             if n != self.position and not self.is_overlapped(n, snakes, grid):
                 self.position = n
                 return
 
-        rand.shuffle(scartati)
-        self.position = scartati[0]
+        rand.shuffle(possible_position)
+        self.position = possible_position[0]
