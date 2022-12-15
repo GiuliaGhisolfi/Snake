2e9ccf2911973fba1d3103c500e3d126cb81c5b4
LucaMaro1 <lucamariniwork@gmail.com>
2022-12-15 18:39:55 +0100

    code cleanup

--- snake.py
+++ snake.py
@@ -3,11 +3,12 @@ import threading
 import copy
 
 class Snake:
-    def __init__(self, color=(0, 190, 80), start_location="top-left"):
-        self.color = color  # immutabile
-        self.start_location = start_location  # da lockare!
+    def __init__(self, color = (0, 190, 80), start_location = "top-left"):
+        self.color = color  # unchangable
+        self.start_location = start_location
 
     def respawn(self, grid):
+        '''respawn the snake at its starting point'''
         self.length = 3
         if self.start_location == "top-left":
             self.body = [(3, 2),
@@ -21,14 +22,16 @@ class Snake:
             self.direction = Directions.UP
     
     def coord_from_graph(self, grid):
+        '''return snake's body coordinates on the grid'''
         body_coord = []
         for node in self.get_body():
             body_coord[len(body_coord):] = [
-                (int(node[0])*grid.block_size, int(node[1])*grid.block_size)]
+                (int(node[0]) * grid.block_size, int(node[1]) * grid.block_size)]
         return body_coord
 
     def draw(self, game, window, grid):
-        # riduciamo il tempo dello snake lockato? o preferiamo la rapidità?
+        '''draw the snake'''
+
         body_coord = self.coord_from_graph(grid)
 
         bsize = grid.block_size
@@ -39,6 +42,7 @@ class Snake:
         game.draw.rect(window, self.color,
                        (tail[0]+1, tail[1]+1, bsize-2, bsize-2))
         prev = (body_coord[0][0], body_coord[0][1])
+
         # Draw the rest of the body
         for segment in body_coord[1:]:
             if segment[0] > prev[0]:
@@ -54,6 +58,7 @@ class Snake:
                 game.draw.rect(window, self.color,
                                (segment[0]+1, segment[1]+1, bsize-2, bsize))
             prev = (segment[0], segment[1])
+        
         # Draw eyes
         black = (0, 0, 0)
         eye_size = bsize/6
@@ -71,8 +76,7 @@ class Snake:
         game.draw.rect(window, black, (eye2_x, eye2_y, eye_size, eye_size))
 
     def move(self, direction, food):
-
-        
+        '''change the position of the snake on the grid'''
 
         self.__steer(direction)
         x, y = self.body[-1]
@@ -98,6 +102,7 @@ class Snake:
         return mangiato
 
     def __steer(self, direction):
+        '''check if the new direction is not the opposite of the current one'''
 
         if direction == None:
             return
@@ -114,23 +119,18 @@ class Snake:
         self.length += 1
 
     def can_eat(self, food):
-        return self.body[-1] == food.position  # confronto tra nodi
+        return self.body[-1] == food.position
 
     def tail_collision(self):
+        '''check if the snake has overlapped its tail'''
         head = self.body[-1]
         for segment in self.body[:-1]:
             if head == segment:
                 return True
         return False
 
-    def adversarial_collision(self, adversarial_body):
-        head = self.body[-1]
-        for segment in adversarial_body:
-            if head == segment:
-                return True
-        return False
-
     def bounds_collision(self, grid):
+        '''check if the snake has overcome the grid bounds'''
         head = self.body[-1]
         if head not in grid.grid:
             return True
@@ -139,96 +139,6 @@ class Snake:
     def get_body(self):
         return copy.deepcopy(self.body)
 
-    def fast_get_body(self):  # attenzione, rompe i principi di Barbara :(
+    def fast_get_body(self):
         return self.body
 
-
-class LockedSnake(Snake):
-    def __init__(self, color=(0, 190, 80), start_location="top-left"):
-
-        super().__init__(color, start_location)
-        self.lock = threading.RLock()
-
-    def respawn(self, grid):
-        self.lock.acquire()
-        try:
-            super().respawn(grid)
-        finally:
-            self.lock.release()
-
-    def draw(self, game, window, grid):
-        # riduciamo il tempo dello snake lockato? o preferiamo la rapidità?
-        self.lock.acquire()
-        try:
-            super().draw(game, window, grid)
-        finally:
-            self.lock.release()
-
-    def move(self, direction):
-        self.lock.acquire()
-        try:
-            super().move(direction)
-        finally:
-            self.lock.release()
-
-    def __steer(self, direction):
-        self.lock.acquire()
-        try:
-            super().__steer(direction)
-        finally:
-            self.lock.release()
-
-    def eat(self):
-        self.lock.acquire()
-        try:
-            super().eat()
-        finally:
-            self.lock.release()
-
-    def can_eat(self, food):
-        self.lock.acquire()
-        try:
-            ret = super().can_eat(food)
-        finally:
-            self.lock.release()
-        return ret  # confronto tra nodi
-
-    def tail_collision(self):
-        self.lock.acquire()
-        try:
-            ret = super().tail_collision()
-        finally:
-            self.lock.release()
-        return ret
-
-    def adversarial_collision(self, adversarial_body):
-        self.lock.acquire()
-        try:
-            ret = super().adversarial_collision(adversarial_body)
-        finally:
-            self.lock.release()
-        return ret
-
-    def bounds_collision(self, grid):
-        self.lock.acquire()
-        try:
-            ret = super().bounds_collision(grid)
-        finally:
-            self.lock.release()
-        return ret
-
-    def get_body(self):
-        self.lock.acquire()
-        try:
-            ret = super().get_body()
-        finally:
-            self.lock.release()
-        return ret
-
-    def fast_get_body(self):
-        self.lock.acquire()
-        try:
-            ret = copy.deepcopy(super().fast_get_body())
-        finally:
-            self.lock.release()
-        return ret
